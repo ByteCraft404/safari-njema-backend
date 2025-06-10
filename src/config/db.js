f662b26d73fb5e3131728926d5bc21dc344e7e50
ByteCraft404 <182875560+ByteCraft404@users.noreply.github.com>
2025-06-10 18:31:30 +0300

    New Db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 const ConnectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("connected to DataBase");
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DataBase");
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: error.message });
+    console.error("Error connecting to MongoDB Atlas:", error.message);
+    throw new Error("Database connection failed: " + error.message);
   }
 };
 export default ConnectToDB;
