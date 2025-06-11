bcabdbb5a0b49eb27aed5c9d8c89f726e17574fb
ByteCraft404 <182875560+ByteCraft404@users.noreply.github.com>
2025-06-11 12:06:16 +0300

    Hello

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://john645-3cdq.onrender.com"
+      'https://safarinjemafrontend.onrender.com'
     ],
     methods: "GET,POST,PUT,DELETE,PATCH",
     allowedHeaders: "Content-Type,Authorization",
